ed953f4491bc3aa40aee0bdb66e331f7d10d5a9c
vinodnaikr <142738355+vinodnaikr@users.noreply.github.com>
2023-10-31 00:26:43 +0530

    Add files via upload

---
+++ sumofn.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+
+    int i;
+    int sum=0;
+    int n;
+    cin>>n;
+    for(int i=0;i<=n;i++)
+    {
+
+        sum=sum+i;
+    }
+    cout<<sum;
+    return 0;
+}
